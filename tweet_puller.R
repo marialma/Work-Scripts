d325349ba647db50a67cc04769e9c8df6875d8f1
Maria Ma <i.am.maria.ma@gmail.com>
2018-04-23 13:37:52 -0700

    Update tweet_puller.R

--- tweet_puller.R
+++ tweet_puller.R
@@ -10,5 +10,5 @@ date <- today - 7
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 tweets = twitteR::searchTwitter('#antibiotics', n = 1e3, since = date, retryOnRateLimit = 1e3)
-df = twitteR::twListToDF(tw)
+df = twitteR::twListToDF(tweets)
 write.csv(df, file = paste(date, "twitter.csv", sep="_"),row.names=FALSE)
