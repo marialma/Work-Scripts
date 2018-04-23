2bd9b2b6a3390ee016b333e214020660421133c5
Maria Ma <i.am.maria.ma@gmail.com>
2018-04-23 13:55:16 -0700

    Update tweet_puller.R
    sigh i went and regenerated all my API keys sigh i am so bad.

--- tweet_puller.R
+++ tweet_puller.R
@@ -4,6 +4,7 @@ consumer_key <- ""
 consumer_secret <- ""
 access_token <- ""
 access_secret <- ""
+#sigh i am garbage
 
 today <- Sys.Date()
 date <- today - 7
@@ -12,3 +13,4 @@ setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 tweets = twitteR::searchTwitter('#antibiotics', n = 1e3, since = date, retryOnRateLimit = 1e3)
 df = twitteR::twListToDF(tweets)
 write.csv(df, file = paste(date, "twitter.csv", sep="_"),row.names=FALSE)
+
