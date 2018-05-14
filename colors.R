844dbeee96ee432e42249807d0ca901f0ce3bae2
Maria Ma <i.am.maria.ma@gmail.com>
2018-05-13 17:09:59 -0700

    colors for work graphs

---
+++ colors.R
@@ -0,0 +1,9 @@
+# Color Scheme
+
+# Base:
+# green, blue, yellow, light yellow-green, light blue, light yellow
+c("#399849", "#3590c0", "#FCB913", "#9AA82F", "#8DBFD5", "#FDE299"))
+
+# More:
+# lighter green, lighter blue, lightest green
+c("#88CA91", "#B3DAF7", "E0F2E2")
